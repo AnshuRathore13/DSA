b48af8d0d539cbc7ce8ea45d577f0ad1f7aaa8c9
AnshuRathore13 <anshurathor989@gmail.com>
2026-08-07 02:35:40 +0530

    Time: 2 ms (60.53%), Space: 41.9 MB (86.62%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int totalNQueens(int n) {
+        count=0;
+        char board[][]=new char[n][n];
+        queen(board,0);
+        return count;
+    }
+    static int count=0;
+    
+    public void queen(char board[][],int row){
+        if(row==board.length){
+            count++;
+            return;
+            
+        }
+
+        for(int j=0;j<board.length;j++){
+            if(isPossible(board,row,j)){
+                board[row][j]='x';
+                queen(board,row+1);
+                board[row][j]='.';
+            }
+            
+        }
+    }
+
+    public boolean isPossible(char board[][],int row,int col){
+        for(int i=row-1;i>=0;i--){
+            if(board[i][col]=='x') return false;
+        }
+
+        for(int i=row-1,j=col-1;i>=0&&j>=0;i--,j--){
+            if(board[i][j]=='x') return false;
+        }
+
+        for(int i=row-1,j=col+1;i>=0&&j<=board.length-1;i--,j++){
+            if(board[i][j]=='x') return false;
+        }
+
+        return true;
+    }
+
+
+
+
+
+
+}
